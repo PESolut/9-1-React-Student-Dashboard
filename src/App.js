8ecd9c5553b98f835c64539ceb4c99e14672c124
PESolut <jahaadpetty@pursuit.org>
2022-12-07 04:45:05 -0500

    AllStudentViews.js - All student view page made, styled

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import { useEffect } from "react";
 import Nav from "./common/Nav";
+import AllStudentViews from "./studentComp/AllStudentViews";
 
 function App() {
+
+
   return (
     <>
-    <Nav/>
+      <Nav/>
+      <AllStudentViews/>
     </>
   );
 }
---
+++ src/studentComp/AllStudentViews.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const AllStudentViews = () => {
+    return (
+        <>
+            <div className='main'>
+                <h2>All Students</h2>
+                    <p>Total Students: </p>
+            </div>
+        </>
+    );
+};
+
+export default AllStudentViews;
